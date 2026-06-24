47c74c2f0f714a8a0d763d9f603db5cfdbf3a071
Aysha Malik <aarzoomalik9540@gmail.com>
2026-06-24 11:48:12 +0530

    Update MongoDB connection string to use placeholders
    Replaced hardcoded MongoDB credentials with placeholders.

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 mongoose.connect(
-  "mongodb://Anjali:Anjali1234@ac-jqemwpb-shard-00-00.epnjwxw.mongodb.net:27017,ac-jqemwpb-shard-00-01.epnjwxw.mongodb.net:27017,ac-jqemwpb-shard-00-02.epnjwxw.mongodb.net:27017/?ssl=true&replicaSet=atlas-iukm9z-shard-0&authSource=admin&appName=Cluster0",
+  "mongodb://<user name>:<user password>@ac-jqemwpb-shard-00-00.epnjwxw.mongodb.net:27017,ac-jqemwpb-shard-00-01.epnjwxw.mongodb.net:27017,ac-jqemwpb-shard-00-02.epnjwxw.mongodb.net:27017/?ssl=true&replicaSet=atlas-iukm9z-shard-0&authSource=admin&appName=Cluster0",
 )
 .then(() => console.log("✅ MongoDB Connected"))
 .catch(err => console.log("❌ MongoDB connection error:", err));
